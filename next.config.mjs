8d9cfc34b11a3f3fe147b5f51a57acd6c20b24b4
msng-desktop <msngch@yahoo.co.jp>
2024-03-28 11:59:49 +0900

    "githubpages6"

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Use the prefix in the Next.js router
-  basePath: '/mshrnggh',
+  basePath: '/mshrnggh/',
   assetPrefix: '/mshrnggh/',
-  // output: 'export', // Remove this line
+  // Add this configuration
+  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/index1': { page: '/index1' },
+      // Add more paths here
+    };
+  },
 };
 
 export default nextConfig;
